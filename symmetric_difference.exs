eda13b91876162ccea60961b08365bd312921e35
Brendan A R Sechter <sgeos@hotmail.com>
2016-10-25 16:53:53 +0900

    Added benchmark.

--- symmetric_difference.exs
+++ symmetric_difference.exs
@@ -23,13 +23,37 @@
 #   ./symmetric_difference.exs
 
 defmodule CustomSet do
-  def symmetricDifference(pSetA, pSetB) do
+  def symmetricDifference_IntersectionDifference(pSetA, pSetB) do
     setA = MapSet.new(pSetA)
     setB = MapSet.new(pSetB)
     union = MapSet.union(setA, setB)
     intersection = MapSet.intersection(setA, setB)
     MapSet.difference(union, intersection)
     |> MapSet.to_list
+  end
+
+  # FEEDBACK:
+  #   benchmark your formula of union(a,b)\intersection(a,b) with union(a\b,b\a)
+  #   just curious about relative costs. does 1U+1I+1D cost less or more than 2D+1U
+  #
+  # ANSWER:
+  #   2D+1U appears to be slightly faster when operating on multiple large sets
+  def symmetricDifference_DifferenceUnion(pSetA, pSetB) do
+    setA = MapSet.new(pSetA)
+    setB = MapSet.new(pSetB)
+    differenceA = MapSet.difference(setA, setB)
+    differenceB = MapSet.difference(setB, setA)
+    MapSet.union(differenceA, differenceB)
+    |> MapSet.to_list
+  end
+end
+
+defmodule Benchmark do
+  def measure(function) do
+    function
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000_000)
   end
 end
 
@@ -39,18 +63,21 @@ defmodule Script do
       token: ",",
       character: false,
       strip: false,
+      benchmark: false,
       help: false,
     ]
     switches = [
       token: :string,
       character: :boolean,
       strip: :boolean,
+      benchmark: :boolean,
       help: :boolean,
     ]
     aliases = [
       t: :token,
       c: :character,
       s: :strip,
+      b: :benchmark,
       "?": :help,
     ]
     {parsed, remaining, invalid} = OptionParser.parse(args, strict: switches, aliases: aliases)
@@ -59,6 +86,8 @@ defmodule Script do
     cond do
       options[:help] or (0 < length invalid) ->
         usage
+      options[:benchmark] ->
+        benchmark
       true ->
         process(remaining, options)
     end
@@ -74,6 +103,8 @@ defmodule Script do
     IO.puts("    -c          : operate on sets of characters instead of sets of strings")
     IO.puts("    --strip     : strip leading and trailing whitespace from set items")
     IO.puts("    -s          : strip leading and trailing whitespace from set items")
+    IO.puts("    --benchmark : perform algorithm benchmark")
+    IO.puts("    -b          : perform algorithm benchmark")
     IO.puts("    --help      : display this usage summary")
     IO.puts("    -?          : display this usage summary")
     IO.puts("Examples:")
@@ -94,9 +125,21 @@ defmodule Script do
     strip = pOptions[:strip]
     pSetList
     |> Enum.map(&stringToSet(&1, token, character, strip))
-    |> Enum.reduce([], &CustomSet.symmetricDifference/2)
+    |> Enum.reduce([], &CustomSet.symmetricDifference_DifferenceUnion/2)
     |> Enum.sort
     |> Enum.each(&IO.puts/1)
+  end
+
+  def benchmark() do
+    range_max = 1000000
+    input_max = 12
+    range = 0..range_max
+    input = 1..input_max
+    |> Enum.map(&(range |> Enum.take_every(&1)))
+    Benchmark.measure(fn -> input |> Enum.reduce([], &CustomSet.symmetricDifference_IntersectionDifference/2) end)
+    |> fn time -> IO.puts("Intersection Difference : #{time} seconds") end.()
+    Benchmark.measure(fn -> input |> Enum.reduce([], &CustomSet.symmetricDifference_DifferenceUnion/2) end)
+    |> fn time -> IO.puts("Difference Union        : #{time} seconds") end.()
   end
 end
 
