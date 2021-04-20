fceb12ac96727666171dd3d9a95624efe39eab53
Brendan A R Sechter <sgeos@hotmail.com>
2021-04-20 19:02:24 +0900

    Converted template to symmetric difference.

--- template.exs
+++ template.exs
@@ -6,30 +6,59 @@
 
 # PROBLEM:
 # Problem description here.
+# This template calculates the symmetric difference of multiple input sets.
 
 defmodule Script do
-  def print_arg(input) do
-    IO.puts("")
-    input
-    |> String.split(":")
-    |> Enum.with_index(1)
-    |> Enum.each(fn {v, i} -> IO.puts("  #{i}: #{v}") end)
+  # core problem logic
+  def symmetric_difference(args) do
+    args
+    |> Enum.reduce(&Kernel.++/2)
+    |> Enum.reduce(%{}, fn i, a -> Map.update(a, i, 1, &(&1 + 1)) end)
+    |> Enum.reduce([],
+      fn
+        {_, v}, a when 0 == rem(v, 2) -> a
+        {k, _}, a -> a ++ [k]
+      end
+    )
+  end
+
+  # reusable printing function
+  def print_set([]), do: IO.puts("  (no members)")
+  def print_set(set) do
+    IO.write("  ")
+    set
+    |> Enum.join(":")
+    |> IO.puts()
   end
 
   # main program logic
-  def run(input) do
-    IO.puts("Input:")
-    input
-    |> Enum.map(&print_arg/1)
+  def run(args) do
+    sets = args
+    |> Enum.map(&String.split(&1, ":"))
+    |> Enum.map(&Enum.uniq/1)
+    IO.puts("Input Sets:")
+    sets
+    |> Enum.each(&print_set/1)
+    IO.puts("Symmetric Difference:")
+    sets
+    |> symmetric_difference()
+    |> print_set()
+    IO.puts("")
   end
 
-  # fixed tests
+  # fixed test cases
   def test() do
     IO.puts("--- USAGE ---")
     main(["help"])
     IO.puts("")
-    IO.puts("--- TEST ---")
-    main(["apple:banana:cat:...", "dog"])
+    IO.puts("--- SIMPLE TESTS ---")
+    main(["a:b:c"])
+    main(["a:b:c", "a:b:c"])
+    main(["a:b:c", "d:e:f"])
+    IO.puts("")
+    IO.puts("--- TESTS ---")
+    main(["a:b:c", "c:d"])
+    main(["a:c:f:m:n", "b:c:j:k:m:n", "c:d:f:k"])
   end
 
   # program usage
@@ -39,11 +68,12 @@ defmodule Script do
     IO.puts("  #{:escript.script_name()} help")
     IO.puts("  #{:escript.script_name()} test")
     IO.puts("")
-    IO.puts("Example:")
-    IO.puts("  #{:escript.script_name()} INPUT")
+    IO.puts("Examples:")
+    IO.puts("  #{:escript.script_name()} a:b:c c:d")
+    IO.puts("  #{:escript.script_name()} a:c:f:m:n b:c:j:k:m:n c:d:f:k")
   end
 
-  # now takes command line arguments
+  # entry point
   def main(args) do
     args = args |> Enum.map(&String.downcase/1)
     cond do
@@ -55,9 +85,9 @@ defmodule Script do
   end
 end
 
-# fixed input
+# call with fixed input
 Script.main(["test"])
 
-# command line input
+# call with command line arguments
 #Script.main(System.argv)
 
